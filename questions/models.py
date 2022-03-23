4b1dc54f61728351072cc63d90ffab510d26352b
Seranddm <and.2001@mail.ru>
2022-03-23 22:34:54 +0300

    Добавил __str__ к моделям. Добавил простейшую панель администратора.

--- questions/models.py
+++ questions/models.py
@@ -7,19 +7,31 @@ class Category(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
     info = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Question(models.Model):
     author = models.ForeignKey(django.contrib.auth.models.User, on_delete = models.CASCADE)
     category = models.ManyToManyField(Category)
-    answer = models.OneToOneField('Answer')
+    answer = models.OneToOneField('Answer', on_delete = models.CASCADE)
     text_of_question = models.TextField()
+
+    def __str__(self):
+        return self.text_of_question
 
 
 class Comment(models.Model):
     author = models.ForeignKey(django.contrib.auth.models.User, on_delete = models.CASCADE)
-    question = models.ForeignKey(Question)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
     text_of_comment = models.TextField()
+
+    def __str__(self):
+        return self.text_of_comment
 
 class Answer(models.Model):
     author = models.ForeignKey(django.contrib.auth.models.User, on_delete = models.CASCADE)
     text_of_answer = models.TextField()
+
+    def __str__(self):
+        return self.text_of_answer
