5009b7da252160027f98aa8e3a3b3184112257d9
Seranddm <and.2001@mail.ru>
2022-03-29 14:04:04 +0300

    Исправил on_delete. Добавил related_name.

--- questions/models.py
+++ questions/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=50)
     info = models.TextField()
 
     def __str__(self):
@@ -12,9 +12,9 @@ class Category(models.Model):
 
 
 class Question(models.Model):
-    author = models.ForeignKey(django.contrib.auth.models.User, on_delete = models.CASCADE)
-    category = models.ManyToManyField(Category)
-    answer = models.OneToOneField('Answer', on_delete = models.CASCADE)
+    author = models.ForeignKey(django.contrib.auth.models.User, default='User', on_delete = models.SET_DEFAULT())
+    category = models.ManyToManyField(Category, related_name='questions')
+    answer = models.OneToOneField('Answer', null=True , blank=True ,on_delete = models.SET_NULL())
     text_of_question = models.TextField()
 
     def __str__(self):
@@ -22,15 +22,15 @@ class Question(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(django.contrib.auth.models.User, on_delete = models.CASCADE)
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    author = models.ForeignKey(django.contrib.auth.models.User, default='User', on_delete = models.SET_DEFAULT())
+    question = models.ForeignKey(Question, on_delete = models.CASCADE, related_name='comments')
     text_of_comment = models.TextField()
 
     def __str__(self):
         return self.text_of_comment
 
 class Answer(models.Model):
-    author = models.ForeignKey(django.contrib.auth.models.User, on_delete = models.CASCADE)
+    author = models.ForeignKey(django.contrib.auth.models.User, default='User', on_delete = models.SET_DEFAULT())
     text_of_answer = models.TextField()
 
     def __str__(self):
